38f3321c354965811c90baa6601fcf7a58e16bbc
Ashutosh <ashutosh@example.com>
2021-05-06 01:19:53 +0545

    setup react routing and components

---
+++ frontend/src/components/navbar.component.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">HOME</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/vending" className="nav-link">Vending Machine</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/stock" className="nav-link">Stock</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
