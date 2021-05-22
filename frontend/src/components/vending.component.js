72fb124c6ce189d8d10f77f8212ba2cab6ed870b
Ashutosh <ashutosh@example.com>
2021-05-23 02:59:11 +0545

    made first version of vending page html ready

--- frontend/src/components/vending.component.js
+++ frontend/src/components/vending.component.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import axios from 'axios';
-
-// const Exercise = props => (
-//   <tr>
-//     <td>{props.exercise.username}</td>
-//     <td>{props.exercise.description}</td>
-//     <td>{props.exercise.duration}</td>
-//     <td>{props.exercise.date.substring(0,10)}</td>
-//     <td>
-//       <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
-//     </td>
-//   </tr>
-// )
 
 export default class Vending extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {exercises: []};
+    this.state = {
+      itemPurchased: 'N/A',
+      pricePaid: 'N/A',
+      returnChange: 'N/A',
+    };
   }
 
   
@@ -26,7 +18,41 @@ export default class Vending extends Component {
   render() {
     return (
       <div>
-        <h3>Vending Machine</h3>
+        <h3>Welcome to Vending Machine</h3> 
+
+        <h4>Items available with price: </h4>
+        <ul>
+          <li>Coke: Rs 20</li>
+          <li>Pepsi: Rs 25</li>
+          <li>Dew: Rs 30</li>
+        </ul>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Select Item you want to get:
+              <br></br>  
+              <select name="item" id="item">
+                <option value="coke">Coke</option>
+                <option value="pepsi">Pepsi</option>
+                <option value="dew">Dew</option>
+              </select>
+            </label>
+            <br></br> 
+            <label>Enter Coins: </label>
+            <br></br> 
+            <input name="coin" id="coin" type="text"></input>
+            <br></br> 
+            <br></br>
+            <input type="submit" value="Submit" />
+            <br></br>
+          </form>
+        </div>
+        <div>
+        <br></br>
+          <h5>Your Transaction Summary</h5>
+          <p>You got one {this.state.itemPurchased} at Rs. {this.state.pricePaid}</p>
+          <p>And received change of Rs. {this.state.returnChange}</p>
+        </div>
       </div>
     )
   }
